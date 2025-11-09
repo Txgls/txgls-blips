6078d136a39fa04de9753cdba7ba9831c80c88e8
Txgls <businesstoggled@gmail.com>
2025-11-09 10:05:54 +0000

    v1.0.0

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Txgls'
+description 'Advanced Blip Management System'
+version '1.0.0'
+
+client_scripts {
+    'client/client.lua'
+}
+
+server_scripts {
+    'server/server.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html'
+}
